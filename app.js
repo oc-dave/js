c35aa72b2efd7663d9d0ac050511b44c9ceef7c8
oc_dave <davidokechukwu18@gmail.com>
2023-11-04 01:18:03 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const wrapper = document.querySelector(".slidderWrapper")
+console.log(wrapper)
+
+const menuItems = document.querySelectorAll(".menuItem")
+
+menuItems.forEach((item,index)=>{
+    item.addEventListener("click", ()=>{
+        wrapper.style.transform =`translateX(${-100 * index}vw)`
+    });
+});
+
